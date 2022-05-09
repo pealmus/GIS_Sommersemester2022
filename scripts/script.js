d1a509440d5f42ff0374cf2ac094be2b617fdad2
pealmus <leon.lucy1997@web.de>
2022-05-09 17:12:37 +0200

    Abgabe4

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,23 @@
-$(function(){
-    $("#form").submit(function(e){
-        // Create data object
-        var data = {};
-        data.name = $("#name").val();
-        data.email = $("#email").val();
-        data.comment = $("#comment").val();
-        // The url of your server-side script that handles the post submission
-        var url = "http://www.someurl.com/post_comment.php";
-        $.POST(url, data)
-        .done(function(response){
-            // This code executes when the server returns a response
-            // Do something with the response like adding the comment to the current list of comments
-            // Example (if your response is HTML, better would be a JSON string):
-            $("#comments").append(response);
-        });
-        e.preventDefault();
-    });
-});
+function speichern() {
+    var kommentare = document.getElementById('kommentare').value;
+    var inhalt = document.getElementById('inhalt')
+    window.localStorage.setItem('kommentare','inhalt');
+    console.log(kommentare);
+    var ausgabe = '';
+    for (var i =0; i < localStorage.length; i++) {
+      ausgabe += localStorage.key(i) + " = " + localStorage.getItem(localStorage.key(i)) + "<br>";
+    }
+    document.getElementById("comments").innerHTML = ausgabe;
+    document.getElementById("kom").innerHTML = Date();
+    document.getElementById("comments").innerHTML = kommentare;  
+
+}
+
+
+
+
+
+
+
+
+
