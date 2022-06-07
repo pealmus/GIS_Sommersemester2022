c980c6f16fd2c7273c6fea501d89470450276d36
pealmus <leon.lucy1997@web.de>
2022-06-07 16:49:46 +0200

    Änderungen

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,58 @@
 function speichern() {
-    var kommentare = document.getElementById('kommentare').value;
-    var inhalt = document.getElementById('inhalt')
+    let kommentare = document.getElementById("kommentare").value;
+    let inhalt = document.getElementById('inhalt');
     window.localStorage.setItem('kommentare','inhalt');
     console.log(kommentare);
-    var ausgabe = '';
-    for (var i =0; i < localStorage.length; i++) {
-      ausgabe += localStorage.key(i) + " = " + localStorage.getItem(localStorage.key(i)) + "<br>";
+    let ausgabe = "";
+    for (let i =0; i < localStorage.length; i++) {
+        ausgabe += localStorage.key(i) + " = " + localStorage.getItem(localStorage.key(i)) + "<br>";
     }
     document.getElementById("comments").innerHTML = ausgabe;
     document.getElementById("kom").innerHTML = Date();
-    document.getElementById("comments").innerHTML = kommentare;  
+    document.getElementById("comments").innerHTML = kommentare;
+}
 
+const tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+const firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
+let player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        videoId: '3DWHZOLozBk',
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+let done = false;
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+    }
+}
+function stopVideo() {
+    player.stopVideo();
 }
 
 
-
-
-
-
-
-
+window.onload = function(){
+let ajax = new XMLHttpRequest();
+ajax.onreadystatechange = function() {
+if(this.readyState === 4 && this.status === 200){
+  document.getElementById("datenschutz").innerHTML = this.response
+}
+}
+ajax.open("GET","datenschutz.html");
+ajax.send();
+}
 
